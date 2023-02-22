2fcb8eb98d254080168974ce8790217fbfe5f5e6
CantarinoG <guilhermecantarino1709@gmail.com>
2023-02-22 13:41:45 -0300

    Set up routing

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('main');
+});
+router.get('/login', function(req, res, next) {
+  res.send('logIn');
+});
+router.get('/signup', function(req, res, next){
+  res.send('signup');
+});
+router.get('/newmessage', function(req, res, next) {
+  res.send('newmessage');
+});
+router.get('/memberstatus', function(req, res, next) {
+  res.send('memberstatus');
 });
 
 module.exports = router;
